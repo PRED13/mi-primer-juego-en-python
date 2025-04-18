13befd34ed5de428da8e39cf82f170cf1e856a90
PRED13 <155868501+PRED13@users.noreply.github.com>
2025-04-17 19:21:43 -0600

    Add files via upload

---
+++ bala.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.sprite import Sprite
+
+class bala(Sprite):
+    def __init__(self, ai_configuraciones, pantalla, nave):
+        super (bala, self).__init__()
+        self.pantalla = pantalla
+        #bala rect en 0,0 y establecer posicion
+        self.rect = pygame.rect(0,0, ai_configuraciones.bala_width,
+                                ai_configuraciones.bala_height)
+        self.rect.centerx = nave.rect.centerx
+        self.rect.top = self.rect.top
+        
+        #valor decimal para la bala
+        self.y = float(self.rect.y)
+        
+        self.color = ai_configuraciones.bala_color
+        self.factor_velocidad = ai_configuraciones.bala_factor_velocidad
